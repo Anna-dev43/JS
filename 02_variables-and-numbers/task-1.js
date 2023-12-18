96eb2b0da18d3379340ea402077524146075f1af
Anna Ivanishcheva <anochka43@yandex.ru>
2023-12-18 19:43:25 +0300

    Практическая работа

---
+++ 02_variables-and-numbers/task-1.js
@@ -0,0 +1,74 @@
+{
+    let x1 = 2;
+    let y1 = 3;
+    
+    let x2 = 10;
+    let y2 = 5;
+    
+    let cathetus1 = Math.abs(x1 - x2);
+    let cathetus2 = Math.abs(y1 - y2);
+    
+    console.log(Math.sqrt(
+      Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+    ));
+    }
+    
+    {
+    let x1 = 10;
+    let y1 = 5;
+    
+    let x2 = 2;
+    let y2 = 3;
+    
+    let cathetus1 = Math.abs(x1 - x2);
+    let cathetus2 = Math.abs(y1 - y2);
+    
+    console.log(Math.sqrt(
+      Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+    ));
+    }
+    
+    {
+    let x1 = -5;
+    let y1 = 8;
+    
+    let x2 = 10;
+    let y2 = 5;
+    
+    let cathetus1 = Math.abs(x1 - x2);
+    let cathetus2 = Math.abs(y1 - y2);
+    
+    console.log(Math.sqrt(
+      Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+    ));
+    }
+    
+    {
+    let x1 = 5;
+    let y1 = 8;
+    
+    let x2 = 5;
+    let y2 = 5;
+    
+    let cathetus1 = Math.abs(x1 - x2);
+    let cathetus2 = Math.abs(y1 - y2);
+    
+    console.log(Math.sqrt(
+      Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+    ));
+    }
+    
+    {
+    let x1 = 8;
+    let y1 = 1;
+    
+    let x2 = 5;
+    let y2 = 1;
+    
+    let cathetus1 = Math.abs(x1 - x2);
+    let cathetus2 = Math.abs(y1 - y2);
+    
+    console.log(Math.sqrt(
+      Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+    ));
+    }
---
+++ 02_variables-and-numbers/task-2.js
@@ -0,0 +1,65 @@
+{
+    let a = 13.123456789;
+    let b = 2.123;
+    let precision = 5;
+  
+    let aNormalized = Math.round(
+      a % 1 * Math.pow(10, precision)
+    );
+  
+    let bNormalized = Math.round(
+      b % 1 * Math.pow(10, precision)
+    );
+  
+    console.log('Исходные числа равны', a === b);
+    console.log('Числа равны', aNormalized === bNormalized);
+    console.log('Первое число больше', aNormalized > bNormalized);
+    console.log('Первое число меньше', aNormalized < bNormalized);
+    console.log('Первое число больше или равно', aNormalized >= bNormalized);
+    console.log('Первое число меньше или равно', aNormalized <= bNormalized);
+    console.log('Числа не равны', aNormalized !== bNormalized);
+  }
+  
+  {
+    let a = 13.890123;
+    let b = 2.891564;
+    let precision = 2;
+  
+    let aNormalized = Math.round(
+      a % 1 * Math.pow(10, precision)
+    );
+  
+    let bNormalized = Math.round(
+      b % 1 * Math.pow(10, precision)
+    );
+  
+    console.log('Исходные числа равны', a === b);
+    console.log('Числа равны', aNormalized === bNormalized);
+    console.log('Первое число больше', aNormalized > bNormalized);
+    console.log('Первое число меньше', aNormalized < bNormalized);
+    console.log('Первое число больше или равно', aNormalized >= bNormalized);
+    console.log('Первое число меньше или равно', aNormalized <= bNormalized);
+    console.log('Числа не равны', aNormalized !== bNormalized);
+  }
+  
+  {
+    let a = 13.890123;
+    let b = 2.891564;
+    let precision = 3;
+  
+    let aNormalized = Math.round(
+      a % 1 * Math.pow(10, precision)
+    );
+  
+    let bNormalized = Math.round(
+      b % 1 * Math.pow(10, precision)
+    );
+  
+    console.log('Исходные числа равны', a === b);
+    console.log('Числа равны', aNormalized === bNormalized);
+    console.log('Первое число больше', aNormalized > bNormalized);
+    console.log('Первое число меньше', aNormalized < bNormalized);
+    console.log('Первое число больше или равно', aNormalized >= bNormalized);
+    console.log('Первое число меньше или равно', aNormalized <= bNormalized);
+    console.log('Числа не равны', aNormalized !== bNormalized);
+  }
