dbed6ddc27b5fabd51226a007fd0922d4672b6f1
Аня Иванищева <anochka43@yandex.ru>
2023-12-19 17:35:14 +0300

    Практическая работа

--- 02_variables-and-numbers/task-2.js
+++ 02_variables-and-numbers/task-2.js
@@ -1,16 +1,11 @@
-{
     let a = 13.123456789;
     let b = 2.123;
     let precision = 5;
-  
-    let aNormalized = Math.round(
-      a % 1 * Math.pow(10, precision)
-    );
-  
-    let bNormalized = Math.round(
-      b % 1 * Math.pow(10, precision)
-    );
-  
+
+    let aNormalized = Math.round(a % 1 * Math.pow(10, precision));
+
+    let bNormalized = Math.round(b % 1 * Math.pow(10, precision));
+
     console.log('Исходные числа равны', a === b);
     console.log('Числа равны', aNormalized === bNormalized);
     console.log('Первое число больше', aNormalized > bNormalized);
@@ -18,21 +13,15 @@
     console.log('Первое число больше или равно', aNormalized >= bNormalized);
     console.log('Первое число меньше или равно', aNormalized <= bNormalized);
     console.log('Числа не равны', aNormalized !== bNormalized);
-  }
-  
-  {
+
     let a = 13.890123;
     let b = 2.891564;
     let precision = 2;
-  
-    let aNormalized = Math.round(
-      a % 1 * Math.pow(10, precision)
-    );
-  
-    let bNormalized = Math.round(
-      b % 1 * Math.pow(10, precision)
-    );
-  
+
+    let aNormalized = Math.round(a % 1 * Math.pow(10, precision));
+
+    let bNormalized = Math.round(b % 1 * Math.pow(10, precision));
+
     console.log('Исходные числа равны', a === b);
     console.log('Числа равны', aNormalized === bNormalized);
     console.log('Первое число больше', aNormalized > bNormalized);
@@ -40,21 +29,15 @@
     console.log('Первое число больше или равно', aNormalized >= bNormalized);
     console.log('Первое число меньше или равно', aNormalized <= bNormalized);
     console.log('Числа не равны', aNormalized !== bNormalized);
-  }
-  
-  {
+
     let a = 13.890123;
     let b = 2.891564;
     let precision = 3;
-  
-    let aNormalized = Math.round(
-      a % 1 * Math.pow(10, precision)
-    );
-  
-    let bNormalized = Math.round(
-      b % 1 * Math.pow(10, precision)
-    );
-  
+
+    let aNormalized = Math.round(a % 1 * Math.pow(10, precision));
+
+    let bNormalized = Math.round(b % 1 * Math.pow(10, precision));
+
     console.log('Исходные числа равны', a === b);
     console.log('Числа равны', aNormalized === bNormalized);
     console.log('Первое число больше', aNormalized > bNormalized);
@@ -62,4 +45,3 @@
     console.log('Первое число больше или равно', aNormalized >= bNormalized);
     console.log('Первое число меньше или равно', aNormalized <= bNormalized);
     console.log('Числа не равны', aNormalized !== bNormalized);
-  }
