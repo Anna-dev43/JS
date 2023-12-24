0a6906136ee6bc0486f9cca10ca963e5d2d305ea
Аня Иванищева <anochka43@yandex.ru>
2023-12-24 13:04:07 +0300

    Практическая работа

--- 04_arrays-and-loops/task-3.js
+++ 04_arrays-and-loops/task-3.js
@@ -1,17 +1,28 @@
-let n = 3;
+/*let n = 3;
 let array = [2,5,1,3,4];
-
-/*let n = 1;
-let array = [5,1,3,2,7,6,4];*/
-
-/*let n = 7;
-let array = [2,1,3];*/
-
-let index = 0;
+let index = 3;
 
 for (let i = 0; i < array.length; i++) {
   if (array[i] == n) {
-    index = 1;
+    break;
+  }
+}*/
+
+/*let n = 1;
+let array = [5,1,3,2,7,6,4];
+let index = 1;
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] == n) {
+    break;
+  }
+}*/
+
+let n = 7;
+let array = [2,1,3];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] == n) {
     break;
   }
 }
