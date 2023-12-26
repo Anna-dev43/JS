0e4fd8be9a8632c7dec23aff66e4cd646e8be008
Аня Иванищева <anochka43@yandex.ru>
2023-12-26 17:58:29 +0300

    Практическая работа

--- 05_functions/task-2.js
+++ 05_functions/task-2.js
@@ -1,15 +1,15 @@
 let whiteList = ['my-email@gmail.ru', 'jsfunc@mail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru', 'goodday@day.ru'];
 let blackList = ['jsfunc@mail.ru','goodday@day.ru'];
-let result = filter(whiteList, blackList);
 
 function filter(whiteList, blackList) {
   let filterList = [];
   for (let i = 0; i < whiteList.length; i++) {
-    if (!blackList[i].includes(whiteList)) {
+    if (!blackList.includes(whiteList[i])) {
       filterList.push(whiteList[i]);
     }
   }
   return filterList;
 }
 
-console.log(filterList);
+let result = filter(whiteList, blackList);
+console.log(result);
