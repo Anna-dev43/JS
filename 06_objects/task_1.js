0749825c4388f0a3874ef37dedf658d6dcfe72b7
Аня Иванищева <anochka43@yandex.ru>
2023-12-30 09:37:42 +0300

    Практическая работа

--- 06_objects/task_1.js
+++ 06_objects/task_1.js
@@ -1,40 +1,67 @@
-let user1={name: 'Игорь', age: 17}
-let user2={name: 'Оля', age: 21}
+let user1 = {name: 'Игорь', age: 17}
+let user2 = {name: 'Оля', age: 21}
 
 function getOlderUser(userOne, userTwo) {
-  if (user1.age > user2.age) {
-    return 'Пользователь' + user1.name + 'старше, ему(ей)' + user1.age
-  } else
-    if (user1.age === user2.age) {
+  if (userOne.age > userTwo.age) {
+    return 'Пользователь ' + userOne.name + ' старше, ему(ей) ' + userOne.age
+  }
+
+  if (userOne.age === userTwo.age) {
       return 'Возраст пользователей равны'
-    }
-    else {
-      return 'Пользователь' + user2.name + 'старше, ему(ей)' + user2.age
-    }
+  }
+
+  return 'Пользователь ' + userTwo.name + ' старше, ему(ей) ' + userTwo.age
 }
 
-let result = getOlderUser(user1, user2)
+let result = getOlderUser(user1, user2);
 console.log(result);
 
+// Вариант решения № 1
 
-/*let allUsers = [
-  {name: 'Валя', age: 11},
-  {name: 'Таня', age: 24},
-  {name: 'Рома', age: 21},
-  {name: 'Надя', age: 34},
-  {name: 'Антон', age: 7}
-]
+// let allUsers = [
+//   {name: 'Валя', age: 11},
+//   {name: 'Таня', age: 24},
+//   {name: 'Рома', age: 21},
+//   {name: 'Надя', age: 34},
+//   {name: 'Антон', age: 7}
+// ]
 
-function getOlderUserArray(users){
-  let oldestUser = 0;
-  let newObject = {};
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].age > oldestUser) {
-      oldestUser = users[i].age;
-      newObject = users[i];
-    }
-  }
-  return newObject.name;
-}
+// function getOlderUserArray(users){
+//   let oldestUser = 0;
+//   let newObject = {};
+//   for (let i = 0; i < users.length; i++) {
+//     if (users[i].age > oldestUser) {
+//       oldestUser = users[i].age;
+//       newObject = users[i];
+//     }
+//   }
+//   return newObject.name;
+// }
 
-console.log(getOlderUserArray(allUsers));*/
+// console.log(getOlderUserArray(allUsers));
+
+
+
+
+// Вариант решения №2
+
+// let allUsers = [
+//   {name: 'Валя', age: 11},
+//   {name: 'Таня', age: 24},
+//   {name: 'Рома', age: 21},
+//   {name: 'Надя', age: 34},
+//   {name: 'Антон', age: 7}
+// ]
+
+// function getOlderUserArray(users){
+//   let oldestUser = users[0];
+
+//   for (let user of users) {
+//     if (user.age > oldestUser.age) {
+//       oldestUser = user;
+//     }
+//   }
+//   return oldestUser.name;
+// }
+
+// console.log(getOlderUserArray(allUsers));
