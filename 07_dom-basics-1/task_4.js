720f127c0bc662a9a7645896f49d702f902ac3ac
Аня Иванищева <anochka43@yandex.ru>
2024-01-09 19:52:47 +0300

    Практическая работа

--- 07_dom-basics-1/task_4.js
+++ 07_dom-basics-1/task_4.js
@@ -1,42 +1,44 @@
-  let allStudents = [
-    { name: 'Валя', age: 11 },
-    { name: 'Таня', age: 24 },
-    { name: 'Рома', age: 21 },
-    { name: 'Надя', age: 34 },
-    { name: 'Антон', age: 7 }
-  ];
+let allStudents = [
+  { name: 'Валя', age: 11 },
+  { name: 'Таня', age: 24 },
+  { name: 'Рома', age: 21 },
+  { name: 'Надя', age: 34 },
+  { name: 'Антон', age: 7 }
+];
 
-  function createStudentCard (student) {
-    let listItem = document.createElement('li');
-    let studentName = document.createElement('h2');
-    let studentYear = document.createElement('span');
+function createStudentCard (student) {
+  let listItem = document.createElement('li');
+  let studentName = document.createElement('h2');
+  let studentYear = document.createElement('span');
 
-    studentName.textContent = student.name;
-    studentYear.textContent = `Возраст: ${student.age} лет`;
+  studentName.textContent = student.name;
+  studentYear.textContent = `Возраст: ${student.age} лет`;
 
-    listItem.append(studentName, studentYear);
+  listItem.append(studentName, studentYear);
 
-    return listItem;
+  return listItem;
+}
+
+function createStudentsList(listArr) {
+  let list = document.createElement('ul');
+  list.style = "list-style: none";
+
+  for (let obj of listArr) {
+    let item = createStudentCard(obj);
+    list.append(item);
   }
 
-  function createStudentsList(listArr) {
-    let ul = document.createElement('ul');
-    ul.style = "list-style: none";
+  document.body.append(list);
+}
 
-    for (let obj of listArr) {
-      let item = createStudentCard(obj);
-      ul.append(item);
-    }
-
-    document.body.append(ul);
-  }
-
-  createStudentsList(allStudents);
+createStudentsList(allStudents);
 
 let btn = document.createElement('button');
+
 document.body.append(btn);
 btn.textContent = 'Показать список';
-btn.addEventListener('click', function() {
-  getElement(allStudents);
-})
+
+btn.onclick = getElement(allStudents)
+
+
 
