25c16f011844194d036278a9cb682aa74efa39bc
Аня Иванищева <anochka43@yandex.ru>
2024-01-20 22:58:18 +0300

    Практическая работа

--- 10_type-transform/main.js
+++ 10_type-transform/main.js
@@ -1,168 +1,117 @@
-let studentsList = [];
+class Students {
+  constructor(name, surename, lastname, birthData, startYear, faculty) {
+    this.name = name;
+    this.surename = surename;
+    this.lastname = lastname;
+    this.birthData = birthData;
+    this.startYear = startYear;
+    this.faculty = faculty;
+  }
 
-// Функция, создающая HTML-элемент для вывода одного студента в таблицу
-function getStudentItem(studentObj) {
-  const tableRow = document.createElement('tr');
-  const nameCell = document.createElement('td');
-  const birthdayCell = document.createElement('td');
-  const startEducationCell = document.createElement('td');
-  const facultyCell = document.createElement('td');
-  const age = calculateAge(studentObj.birthday);
-  const startYear = parseInt(studentObj.startEducation);
+  get fio() {
+    return this.surename + " " + this.name + " " + this.lastname;
+  }
 
-  // Добавить информацию о студенте в создание элементы HTML
-  nameCell.textContent = `${studentObj.lastname} ${studentObj.name} ${studentObj.surname}`;
-  birthdayCell.textContent = `${studentObj.birthday.split('-').reverse().join('.')} (${age} лет)`;
-  startEducationCell.textContent = getEducationPeriod(startYear);
-  facultyCell.textContent = studentObj.faculty;
+  getStudPeriod() {
+    const currentTime = new Date();
+    return currentTime.getFullYear() - this.startYear;
+  }
 
-  // Добавление созданных элементов HTML в строку таблицы
-  tableRow.appendChild(nameCell)
-  tableRow.appendChild(birthdayCell)
-  tableRow.appendChild(startEducationCell)
-  tableRow.appendChild(facultyCell)
+  getBirthDataString() {
+    console.log(this.birthData) 
 
-  return tableRow;
-};
+    const yyyy = this.birthData.getFullYear();
+    let mm = this.birthData.getMonth() + 1;
+    let dd = this.birthData.getDate();
 
-// Функция, отображения таблицы со списком студентов
-let studentArray = [];
-function renderStudentsTable() {
-  const tableBody = document.querySelector('#students-list'); // тело таблицы
-  const fragment = document.createDocumentFragment(); // создание фрагмента для таблицы со списком студентов
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
 
-  // Создание строк таблицы с данными студентами, в том числе отфильтрованных, и дабавление их во фрагмент
+    return dd + "." + mm + "." + yyyy;
+  }
+}
 
-  const nameFilter = filterForm.elements['name-filter'].value.trim();
-  const facultyFilter = filterForm.elements['faculty-filter'].value.trim();
-  const startEducFilter = filterForm.elements['start-educ-filter'].value.trim();
-  const finishEducFilter = filterForm.elements['finish-educ-filter'].value.trim();
-  const filters = {
-    name: nameFilter,
-    faculty: facultyFilter,
-    startEducation: startEducFilter,
-    finishEducation: finishEducFilter
-  };
-  studentArray = filterStudentsArray(studenstList, filters);
-  studentArray.forEach(student => {
-    const rowContent = getStudentItem(student);
-    fragment.appendChild(rowContent);
+// Массив сотрудников
+const stud = [
+  new Students("Игорь", "Фролов", "Сергеевич", new Date(1992, 1, 21), 2015, "Физика"),
+  new Students("Алена", "Белых", "Юрьевна", new Date(1995, 3, 15), 2020, "Химия"),
+  new Students("Иван", "Ушаков", "Владимирович", new Date(1900, 10, 3), 2010, "Биология"),
+];
+
+const $studentsList = document.getElementById("students-list"),
+  $studentsListTHAll = document.querySelectorAll(".studentsTable th");
+
+let column = "fio",
+  columnDir = true;
+
+// Поличить TR сотрудника
+function newStudentTR(student) {
+  const $studentTR = document.createElement("tr"),
+    $fioTD = document.createElement("td"),
+    $birthDataTD = document.createElement("td"),
+    $startYearTD = document.createElement("td"),
+    $facultyTD = document.createElement("td");
+
+  $fioTD.textContent = student.fio;
+  $birthDataTD.textContent = student.getBirthDataString();
+  $startYearTD.textContent = student.startYear + " (" + student.getStudPeriod() + " лет)";
+  $facultyTD.textContent = student.faculty;
+
+  $studentTR.append($fioTD);
+  $studentTR.append($birthDataTD);
+  $studentTR.append($startYearTD);
+  $studentTR.append($facultyTD);
+
+  return $studentTR;
+}
+
+// получить сортировку массива по параметрам
+function getSortStudents(prop, dir) {
+  const studCopy = [...stud];
+  return studCopy.sort(function (studentA, studentB) {
+    if (!dir == false ? studentA[prop] < studentB[prop] : studentA[prop] > studentB[prop])
+      return -1;
   });
+}
 
-// Очистка тела таблицы и добавление фрагмента со строками таблицы
-  tableBody.innerHTML = '';
-  tableBody.appendChild(fragment);
+// Отрисовать
+function render() {
+  let studCopy = [...stud];
 
-// Ограничение максимальной даты рождения на текущую дату
-  const birthdayInput = document.getElementById('birthday');
-  const maxDate = new Date().toISOString().split('T')[0];
-  birthdayInput.setAttribute('max', maxDate);
+  studCopy = getSortStudents(column, columnDir);
 
-// Ограничение максимального года начала обучения на текущий год
-  const startEducationInput = document.getElementById('startEducation');
-  const maxYear = new Date().getFullYear();
-  startEducationInput.setAttribute('max', maxYear);
+  $studentsList.innerHTML = "";
 
-// Добавление обработчиков событий на загаловки таблицы
-  const nameHeader = document.querySelector('#name-header');
-  nameHeader.addEventListener('click', () => {
-    sortStudentsByName(studentsList);
-    renderStudentsTable();
+  for (const student of studCopy) $studentsList.append(newStudentTR(student));
+}
+
+// События сотрировки
+$studentsListTHAll.forEach((elment) => {
+  elment.addEventListener("click", function () {
+    column = this.dataset.column;
+    columnDir = !columnDir;
+    render();
   });
-
-  const birthdayHeader = document.querySelector('#birthday-header');
-  birthdayHeader.addEventListener('click', () => {
-    sortStudentsByBirthday(studentsList);
-    renderStudentsTable();
-  });
-
-  const startEducationHeader = document.querySelector('#start__education-header');
-  startEducationHeader.addEventListener('click', () => {
-    sortStudentsByBirthday(studentsList);
-    renderStudentsTable();
-  });
-
-  const facultyHeader = document.querySelector('#facultyHeader');
-  facultyHeader.addEventListener('click', () => {
-    sortStudentsByBirthday(studentsList);
-    renderStudentsTable();
-  });
-};
-
-// К форме добавления студента добавлен слушатель события отправки формы, в которой будет проверка введенных данных
-  const addStudentForm = document.querySelector('#add-student-form');
-  addStudentForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-  const surname = addStudentForm.elements.surname.value.trim();
-  const name = addStudentForm.elements.name.value.trim();
-  const lastname = addStudentForm.elements.lastname.value.trim();
-  const birthday = addStudentForm.elements.birthday.value.trim();
-  const startEducation = addStudentForm.elements.startEducation.value.trim();
-  const faculty = addStudentForm.elements.faculty.value.trim();
-
-// Если проверка пройдет успешно, добавить объект с данными студентов в массив и запустить функцию отрисовки таблицы
-    if (surname && name && lastname && birthday && startEducation && faculty) {
-      const newStudent = {
-        surname,
-        name,
-        lastname,
-        birthday,
-        startEducation,
-        faculty
-      };
-
-    studentsList.push(newStudent);
-    localStorage.setItem('studentsList', JSON.stringify(studentsList));
-    renderStudentsTable();
-    addStudentForm.requestFullscreen();
-    }
 });
 
+// Добавление
+document
+  .getElementById("add-student")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// Фильтрация
+    stud.push(
+      new Students(
+        document.getElementById("input-name").value,
+        document.getElementById("input-surename").value,
+        document.getElementById("input-lastname").value,
+        new Date(document.getElementById("input-birthData").value),
+        Number(document.getElementById("input-startYear").value),
+        document.getElementById("input-faculty").value
+      )
+    );
 
-// Function to render filtered students
-function renderFilteredStudentsTable(filterStudents) {
-  const tableBody = document.querySelector('#students-list');
-  const fragment = document.createDocumentFragment();
-
-  // Clearing table content
-  tableBody.innerHTML = '';
-
-  filterStudents.forEach(student => {
-    const rowContent = getStudentItem(student);
-    fragment.appendChild(rowContent);
+    render();
   });
 
-  tableBody.appendChild(fragment);
-}
-
-// Function to filter students based on criteria
-function filteredStudentsTable(studenstList) {
-  let filteredStudentsList = [...studenstList];
-  const nameFilter = document.getElementById('name-filter').value.trim();
-  const facultyFilter = document.getElementById('faculty-filter').value.trim();
-  const startEducFilter = document.getElementById('star-educ-filter').value.trim();
-  const finishEducFilter = document.getElementById('finish-educ-filter').value.trim();
-
-if (!!nameFilter) filteredStudentsList = filterStudentsArray(filteredStudentsList, 'name', nameFilter);
-if (!!facultyFilter) filteredStudentsList = filterStudentsArray(filteredStudentsList, 'faculty', facultyFilter);
-if (!!startEducFilter) filteredStudentsList = filterStudentsArray(filteredStudentsList, 'startEducation',startEducFilter);
-if (!!finishEducFilter) filteredStudentsList = filterStudentsArray(filteredStudentsList, 'finishEducation', finishEducFilter);
-
-return filteredStudentsList;
-}
-
-function sortStudentsTable(studenstList) {
-  let sortedStudentsList = [...studenstList];
-  const tableHeaders = document.getElementsByClassName('table__header-title');
-  for(const tableHeader of tableHeaders) {
-    if(tableHeader.classList.contains('sorted')) {
-      sortedStudentsList = sortStudents(studenstList, bindSortValues(tableHeader.id),true);
-    }
-  }
-  return sortedStudentsList;
-}
-
-
+render();
