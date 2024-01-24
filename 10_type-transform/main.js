069df7520a83aabedaa1f001aa1dfbcd96a7f6e7
Аня Иванищева <anochka43@yandex.ru>
2024-01-24 14:22:50 +0300

    Практическая работа

--- 10_type-transform/main.js
+++ 10_type-transform/main.js
@@ -30,46 +30,49 @@ sortDirFlag = true
 
 // Создание элементов
 const $app = document.getElementById('app'),
-$addForm = document.getElementById('add-form'),
-$nameInp = document.getElementById('add-form__name-inp'),
-$surenameInp = document.getElementById('add-form__surename-inp'),
-$lastnameInp = document.getElementById('add-form__lastname-inp'),
-$ageInp = document.getElementById('add-form__age-inp'),
-$educationInp = document.getElementById('add-form__education-inp')
-$facultyInp = document.getElementById('add-form__faculty-inp'),
-$sortFIOBtn = document.getElementById('sort__fio'),
-$sortAgeBtn = document.getElementById('sort__age'),
+      $addForm = document.getElementById('add-form'),
+      $nameInp = document.getElementById('add-form__name-inp'),
+      $surenameInp = document.getElementById('add-form__surename-inp'),
+      $lastnameInp = document.getElementById('add-form__lastname-inp'),
+      $ageInp = document.getElementById('add-form__age-inp'),
+      $educationInp = document.getElementById('add-form__education-inp'),
+      $facultyInp = document.getElementById('add-form__faculty-inp'),
+      $sortFIOBtn = document.getElementById('sort__fio'),
+      $sortAgeBtn = document.getElementById('sort__age'),
 
-$filterForm = document.getElementById('filter-form'),
-$fioFilterInp = document.getElementById('filter-form__fio-inp'),
-$facultyFilterInp = document.getElementById('filter-form__faculty-inp'),
+      $filterForm = document.getElementById('filter-form'),
+      $fioFilterInp = document.getElementById('filter-form__fio-inp'),
+      $facultyFilterInp = document.getElementById('filter-form__faculty-inp'),
 
-$table = document.createElement('table'),
-$tableHead = document.createElement('thead'),
-$tableBody = document.createElement('tbody'),
+      $table = document.createElement('table'),
+      $tableHead = document.createElement('thead'),
+      $tableBody = document.createElement('tbody'),
 
-$tableHeadTr = document.createElement('tr'),
-$tableHeadThFIO = document.createElement('th'),
-$tableHeadThAge = document.createElement('th'),
-$tableHeadThBirthYear = document.createElement('th'),
-$tableHeadThFaculty = document.createElement('th');
+      $tableHeadTr = document.createElement('tr'),
+      $tableHeadThFIO = document.createElement('th'),
+      $tableHeadThAge = document.createElement('th'),
+      $tableHeadThBirthYear = document.createElement('th'),
+      $tableHeadThEducation = document.createElement('th'),
+      $tableHeadThFaculty = document.createElement('th');
 
-$table.classList.add('table', 'table-dark')
+      $table.classList.add('table')
 
-$tableHeadThFIO.textContent = 'ФИО'
-$tableHeadThAge.textContent = 'Возраст'
-$tableHeadThBirthYear.textContent = 'Год рождения'
-$tableHeadThFaculty.textContent = 'Факультет'
+      $tableHeadThFIO.textContent = 'ФИО'
+      $tableHeadThAge.textContent = 'Возраст'
+      $tableHeadThBirthYear.textContent = 'Год рождения'
+      $tableHeadThEducation.textContent = 'Начало обучения'
+      $tableHeadThFaculty.textContent = 'Факультет'
 
-$tableHeadTr.append($tableHeadThFIO)
-$tableHeadTr.append($tableHeadThAge)
-$tableHeadTr.append($tableHeadThBirthYear)
-$tableHeadTr.append($tableHeadThFaculty)
+      $tableHeadTr.append($tableHeadThFIO)
+      $tableHeadTr.append($tableHeadThAge)
+      $tableHeadTr.append($tableHeadThBirthYear)
+      $tableHeadTr.append($tableHeadThEducation)
+      $tableHeadTr.append($tableHeadThFaculty)
 
-$tableHead.append($tableHeadTr)
-$table.append($tableHead)
-$table.append($tableBody)
-$app.append($table)
+      $tableHead.append($tableHeadTr)
+      $table.append($tableHead)
+      $table.append($tableBody)
+      $app.append($table)
 
 // Создание Tr одного пользователя
 function createUserTr(oneUser) {
